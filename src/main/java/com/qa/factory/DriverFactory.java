a3ab5dbc2430ff162e69244b19c3ccc8b745ffc9
sanman <learnsanjeev@gmail.com>
2022-08-02 09:08:34 -0400

    Update DriverFactory.java
    Initialize the threadlocal and write the logic to set the driver (chrome, firefox or safari from drivermanager) to thread local. Also write the method to get the threadlocal .

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -1,5 +1,37 @@
 package com.qa.factory;
 
 public class DriverFactory {
+  
+  public WebDriver driver;
+  
+  public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+  
+  public void init_driver(String browser) {
+    
+    System.out.println("browser value is: " + browser);
+    
+    if(browser.equals("chrome")){
+      WebdriverManager.chromedriver().setup();
+      tlDriver.set(new ChromeDriver());
+    }
+    else if(browser.equals("firefox")){
+      WebdriverManager.firefoxdriver().setup();
+      tlDriver.set(new FirefoxDriver());
+    }
+    else if(browser.equals("safari")){
+      tlDriver.set(new SafariDriver()); 
+    }
+    else {
+      System.out.println("Please enter the correct browser value: " + browser)
+    }
+    
+    getDriver().manage().deleteAllCookies();
+    getDriver().manage().window().maximize();
+    return getDriver();
+  }
+  
+  public static webDriver getDriver() {
+    return tlDriver.get();
+  }
 
 }
